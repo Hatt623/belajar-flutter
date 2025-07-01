328e0c467761945cfdeade07f6c828eaeba5d99d
Hatt623 <mrplanet623@gmail.com>
2025-07-01 11:53:29 +0700

    07/01/2025

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:Scaffold(
+
+        appBar: AppBar(
+          title: Text('Belajar Flutter', 
+            style: TextStyle(fontSize: 24, color: Color.fromARGB(255, 94, 225, 216)),
+          ),
+          centerTitle: true,
+          backgroundColor: const Color.fromARGB(255, 97, 99, 102),
+        ),
+
+        body: Container( 
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [const Color.fromARGB(255, 127, 129, 133), const Color.fromARGB(255, 172, 171, 176)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+
+          child: Container(
+            margin: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.amber,
+              borderRadius: BorderRadius.circular(8),
+              gradient: LinearGradient(
+                colors: [Color.fromARGB(255, 152, 163, 168), Color.fromARGB(255, 173, 193, 196), Colors.white],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+
+            child: Center(
+              child: Text(
+                'Hello flutter',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
